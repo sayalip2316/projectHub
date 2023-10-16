1d3f8e36b84a1b65fc935372687bbaed35a22fcf
SAYALI JADHAV <jadhavsayalip2316@gmail.com>
2023-10-16 12:02:03 +0530

    done

---
+++ Backend/Config/db.js
@@ -0,0 +1,5 @@
+const mongoose=require("mongoose");
+
+const connection=mongoose.connect("mongodb+srv://sayali:sayalijadhav@cluster0.vgf4quo.mongodb.net/projectHub?retryWrites=true&w=majority");
+
+module.exports={connection}
